1ad20c34995c82efc21e0000fac067617191685a
Pietro Deandrea <49197712+PietroDND@users.noreply.github.com>
2025-05-26 20:36:51 +0200

    Edited dist path

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,10 @@ app.use("/api/chats", chatsRoutes);
 app.use("/api/chats/:chatId/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
     
     app.get('/{*any}', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../frontend', 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
     });
 }
 
